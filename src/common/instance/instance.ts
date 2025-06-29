9c955d6f429a56c97b3672d205317e7456c00380
Victor <viktordenisevich6@gmail.com>
2025-06-29 12:56:34 +0300

    Update instance.ts

--- src/common/instance/instance.ts
+++ src/common/instance/instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const token = '68e45cb4-ad1f-4ab1-87c1-7fe4745582b4'
-const apiKey = '5d2e37f1-395e-4105-b8eb-f1ffe52ce33f'
+const token = ''
+const apiKey = ''
 
 export const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
