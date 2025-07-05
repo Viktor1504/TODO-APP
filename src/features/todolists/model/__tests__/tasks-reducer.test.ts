2b918d04ac0826325207fa4af97302437bfa5d7f
Viktor1504 <viktordenisevich6@gmail.com>
2025-07-06 00:55:16 +0300

    refactored test

--- src/features/todolists/model/__tests__/tasks-reducer.test.ts
+++ src/features/todolists/model/__tests__/tasks-reducer.test.ts
@@ -72,22 +72,20 @@ test('array should be created for new todolist', () => {
     const newTodolist: DomainTodolist = {id: nanoid(), title, addedDate: 'testText', order: 2, filter: 'all'}
     const endState = tasksReducer(startState, createTodolistTC.fulfilled({todolist: newTodolist}, 'requestId2', title))
 
-    const keys = Object.keys(endState)
-    const newKey = keys.find((k) => k !== 'todolistId1' && k !== 'todolistId2')
-    if (!newKey) {
-        throw Error('New key should be added')
-    }
+    const newKey = Object.keys(endState).find(key => key !== 'todolistId1' && key !== 'todolistId2')
 
-    expect(keys.length).toBe(3)
-    expect(endState[newKey]).toEqual([])
+    // Добавил проверку, что новый ключ добавлен
+    expect(newKey).toBeDefined() // Убедитесь, что новый ключ найден
+    expect(endState[newKey!]).toEqual([]) // Ожидаем, что новый ключ будет пустым массивом
+    expect(Object.keys(endState).length).toBe(3) // Проверяем, что всего три ключа
 })
 
 test('property with todolistId should be deleted', () => {
     const endState = tasksReducer(startState, deleteTodolistTC.fulfilled({id: 'todolistId2'}, 'requestId', 'todolistId2'))
-    const keys = Object.keys(endState)
 
-    expect(keys.length).toBe(1)
-    expect(endState['todolistId2']).toBeUndefined()
+    // Добавил проверку на удаление данных для конкретного тудулиста
+    expect(endState['todolistId2']).toBeUndefined() // Убедитесь, что ключ удален
+    expect(Object.keys(endState).length).toBe(1) // Проверка на то, что остался только один ключ
 })
 
 test('correct task should be deleted', () => {
@@ -96,47 +94,9 @@ test('correct task should be deleted', () => {
         taskId: '2'
     }, 'requestId', {todolistId: 'todolistId2', taskId: '2'}))
 
-    expect(endState).toEqual({
-        todolistId1: [
-            {
-                id: '1',
-                title: 'CSS',
-                status: TaskStatus.New,
-                todoListId: 'todolistId1',
-                ...taskDefaultValues,
-            },
-            {
-                id: '2',
-                title: 'JS',
-                status: TaskStatus.Completed,
-                todoListId: 'todolistId1',
-                ...taskDefaultValues,
-            },
-            {
-                id: '3',
-                title: 'React',
-                status: TaskStatus.New,
-                todoListId: 'todolistId1',
-                ...taskDefaultValues,
-            },
-        ],
-        todolistId2: [
-            {
-                id: '1',
-                title: 'bread',
-                status: TaskStatus.New,
-                todoListId: 'todolistId2',
-                ...taskDefaultValues,
-            },
-            {
-                id: '3',
-                title: 'tea',
-                status: TaskStatus.New,
-                todoListId: 'todolistId2',
-                ...taskDefaultValues,
-            },
-        ],
-    })
+    // Проверка, что удаленная задача больше не присутствует и что длина массива уменьшена
+    expect(endState['todolistId2'].length).toBe(2) // Проверяем, что в todolistId2 теперь 2 задачи
+    expect(endState['todolistId2'].find(task => task.id === '2')).toBeUndefined() // Проверяем, что задача с id '2' удалена
 })
 
 test('correct task should be created at correct array', () => {
@@ -146,11 +106,12 @@ test('correct task should be created at correct array', () => {
         title: 'juice'
     }))
 
-    expect(endState.todolistId1.length).toBe(3)
-    expect(endState.todolistId2.length).toBe(4)
-    expect(endState.todolistId2[0].id).toBeDefined()
-    expect(endState.todolistId2[0].title).toBe('juice')
-    expect(endState.todolistId2[0].status).toBe(TaskStatus.New)
+    // Проверка, что задача была создана и добавлена в правильный массив
+    expect(endState.todolistId2.length).toBe(4) // Убедитесь, что в todolistId2 теперь 4 задачи
+    const createdTask = endState.todolistId2.find(task => task.title === 'juice')
+    expect(createdTask).toBeDefined() // Убедитесь, что задача создана
+    expect(createdTask?.id).toBe('4') // Убедитесь, что id правильный
+    expect(createdTask?.status).toBe(TaskStatus.New) // Убедитесь, что статус правильный
 })
 
 test('correct task should change its status', () => {
@@ -170,8 +131,11 @@ test('correct task should change its status', () => {
         }),
     )
 
+    // Добавил проверки на то, что задачи не изменились
     expect(endState['todolistId2'][1].status).toBe(TaskStatus.New)
     expect(endState['todolistId1'][1].status).toBe(TaskStatus.Completed)
+    expect(endState['todolistId1'][0].status).toBe(TaskStatus.New) // Убедитесь, что другие задачи не изменились
+    expect(endState['todolistId1'][2].status).toBe(TaskStatus.New) // Убедитесь, что другие задачи не изменились
 })
 
 test('correct task should change its title', () => {
@@ -191,6 +155,8 @@ test('correct task should change its title', () => {
         }),
     )
 
-    expect(endState['todolistId2'][1].title).not.toBe('tea')
-    expect(endState['todolistId1'][1].title).toBe('JS')
+    // Проверка изменения только той задачи, которая была изменена
+    expect(endState['todolistId2'][1].title).toBe('juice') // Проверяем, что название изменилось
+    expect(endState['todolistId2'][1].title).not.toBe('tea') // Убедитесь, что не вернулось старое название
+    expect(endState['todolistId1'][1].title).toBe('JS') // Убедитесь, что другие задачи не изменились
 })
