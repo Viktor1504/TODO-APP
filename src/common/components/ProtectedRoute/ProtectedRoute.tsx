cd4048c9a0f0bd128ebaf63c91a72c474e090f2f
Viktor1504 <viktordenisevich6@gmail.com>
2025-07-08 22:13:46 +0300

    ProtectedRoute refactor

--- src/common/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/common/components/ProtectedRoute/ProtectedRoute.tsx
@@ -4,11 +4,12 @@ import { PropsWithChildren } from 'react'
 
 type Props = PropsWithChildren<{
   isAllowed: boolean
+  redirectPath: string
 }>
 
-export const ProtectedRoute = ({ isAllowed, children }: Props) => {
+export const ProtectedRoute = ({ isAllowed, children, redirectPath = Path.Login }: Props) => {
   if (!isAllowed) {
-    return <Navigate to={Path.Login} />
+    return <Navigate to={redirectPath} />
   }
 
   return children ? children : <Outlet />
