e9785d370376cb500070fef2951e712914178a5b
Viktor1504 <viktordenisevich6@gmail.com>
2025-07-12 22:08:57 +0300

    add middleware

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,6 +3,8 @@ import { appReducer, appSlice } from '@/app/appSlice.ts'
 import { todolistsReducer, todolistsSlice } from '@/features/todolists/model/todolistsSlice.ts'
 import { tasksReducer, tasksSlice } from '@/features/todolists/model/tasksSlice.ts'
 import { authReducer, authSlice } from '@/features/auth/model/authSlice.ts'
+import { todolistsApi } from '@/features/todolists/api/todolistsApi.ts'
+import { setupListeners } from '@reduxjs/toolkit/query'
 
 // создание store
 export const store = configureStore({
@@ -11,8 +13,12 @@ export const store = configureStore({
     [todolistsSlice.name]: todolistsReducer,
     [tasksSlice.name]: tasksReducer,
     [authSlice.name]: authReducer,
+    [todolistsApi.reducerPath]: todolistsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(todolistsApi.middleware),
 })
+
+setupListeners(store.dispatch)
 
 // автоматическое определение типа всего объекта состояния
 export type RootState = ReturnType<typeof store.getState>
