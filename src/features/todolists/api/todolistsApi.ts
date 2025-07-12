370ff3d5993b3ba29f2b1f96b87ca1dac1285d79
Viktor1504 <viktordenisevich6@gmail.com>
2025-07-12 22:03:08 +0300

    add createApi

--- src/features/todolists/api/todolistsApi.ts
+++ src/features/todolists/api/todolistsApi.ts
@@ -1,19 +1,31 @@
-import { Todolist } from '@/features/todolists/api/todolistsApi.types.ts'
-import { BaseResponse } from '@/common/types'
-import { instance } from '@/common/instance'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { AUTH_TOKEN } from '@/common/constants'
 
-export const todolistsApi = {
-  getTodolists() {
-    return instance.get<Todolist[]>('todo-lists')
-  },
-  createTodolist(title: string) {
-    return instance.post<BaseResponse<{ item: Todolist }>>('todo-lists', { title })
-  },
-  deleteTodolist(id: string) {
-    return instance.delete<BaseResponse>(`todo-lists/${id}`)
-  },
-  changeTodolistTitle(payload: { id: string; title: string }) {
-    const { id, title } = payload
-    return instance.put<BaseResponse>(`todo-lists/${id}`, { title }, {})
-  },
-}
+// `createApi` - функция из `RTK Query`, позволяющая создать объект `API`
+// для взаимодействия с внешними `API` и управления состоянием приложения
+export const todolistsApi = createApi({
+  // `reducerPath` - имя `slice`, куда будут сохранены состояние и экшены для этого `API`
+  reducerPath: 'todolistsApi',
+  // `baseQuery` - конфигурация для `HTTP-клиента`, который будет использоваться для отправки запросов
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_BASE_URL,
+    prepareHeaders: (headers) => {
+      headers.set('API-KEY', import.meta.env.VITE_API_KEY)
+      headers.set('Authorization', `Bearer ${localStorage.getItem(AUTH_TOKEN)}`)
+    },
+  }),
+  // `endpoints` - метод, возвращающий объект с эндпоинтами для `API`, описанными
+  // с помощью функций, которые будут вызываться при вызове соответствующих методов `API`
+  // (например `get`, `post`, `put`, `patch`, `delete`)
+  endpoints: (build) => ({
+    // Типизация аргументов (<возвращаемый тип, тип query аргументов (`QueryArg`)>)
+    // `query` по умолчанию создает запрос `get` и указание метода необязательно
+    getTodolists: build.query<any[], void>({
+      query: () => 'todo-lists',
+    }),
+  }),
+})
+
+// `createApi` создает объект `API`, который содержит все эндпоинты в виде хуков,
+// определенные в свойстве `endpoints`
+export const { useGetTodolistsQuery } = todolistsApi
