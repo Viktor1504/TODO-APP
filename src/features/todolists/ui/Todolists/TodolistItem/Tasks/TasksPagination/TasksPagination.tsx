2d17415c640aa263d64aab3614d1c0e35344a45a
Viktor1504 <viktordenisevich6@gmail.com>
2025-07-31 23:42:49 +0300

    refactor Pagination

--- src/features/todolists/ui/Todolists/TodolistItem/Tasks/TasksPagination/TasksPagination.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/Tasks/TasksPagination/TasksPagination.tsx
@@ -10,6 +10,7 @@ type Props = {
 }
 
 export const TasksPagination = ({ totalCount, page, setPage }: Props) => {
+  const paginationCount = totalCount > 0 ? Math.ceil(totalCount / PAGE_SIZE) : 0
   const changePage = (_: ChangeEvent<unknown>, page: number) => {
     setPage(page)
   }
@@ -17,11 +18,15 @@ export const TasksPagination = ({ totalCount, page, setPage }: Props) => {
   return (
     <>
       <Pagination
-        count={Math.ceil(totalCount / PAGE_SIZE)}
+        count={paginationCount}
         page={page}
         color="primary"
         onChange={changePage}
-        sx={{ display: 'flex', justifyContent: 'center' }}
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          visibility: paginationCount >= 2 ? 'visible' : 'hidden',
+        }}
       />
       <div>
         <Typography variant="caption">Total: {totalCount}</Typography>
