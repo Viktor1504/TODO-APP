f02d9fe6537a173fe9a427c673bbc6cc4bfa0f7d
Viktor1504 <viktordenisevich6@gmail.com>
2025-08-04 20:39:48 +0300

    refactor login

--- src/features/auth/lib/schemas/loginSchema.ts
+++ src/features/auth/lib/schemas/loginSchema.ts
@@ -1,11 +1,11 @@
 import { z } from 'zod/v4'
 
 export const loginSchema = z.object({
-  email: z.email({ message: 'Некорректный адрес электронной почты' }),
+  email: z.email({ message: 'Некорректный адрес электронной почты!' }),
   password: z
     .string()
-    .min(1, { message: 'Пароль обязателен для заполнения' })
-    .min(3, { message: 'Пароль должен содержать минимум 3 символа' }),
+    .min(1, { message: 'Пароль обязателен для заполнения!' })
+    .min(3, { message: 'Пароль должен содержать минимум 3 символа!' }),
   rememberMe: z.boolean().optional(),
   captcha: z.string().optional(),
 })
